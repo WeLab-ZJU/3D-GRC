4dd7e5e26c1e7f4e85e53483dad92c4cb9d8f99d
xhb1514288815 <67776806+xhb1514288815@users.noreply.github.com>
2026-04-30 19:33:33 +0800

    Update compile.py

--- Cuda_kernal/compile.py
+++ Cuda_kernal/compile.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     setup(
         name='GRC_Rendering',
         ext_modules=[
-            CUDAExtension('CUDA_kernal_3dgrc', [
-                'GRC_Rendering_Interface.cpp', 
-                'GRC_Rendering_cuda.cu',             # 需要包含.cpp和.cu文件
+            CUDAExtension('CUDA_kernal_3dagr', [
+                'AGR_Rendering_Interface.cpp', 
+                'AGR_Rendering_cuda.cu',             # 需要包含.cpp和.cu文件
             ]),
         ],
         cmdclass={
